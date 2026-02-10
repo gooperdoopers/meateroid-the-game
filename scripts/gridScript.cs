f950b7711daf6c89d41917dded0358f2754a4b7d
the first ever commitment <scrabnabmalfunctions@gmail.com>
2026-02-09 22:27:21 -0800

    matrixier matrixer matrix
    more matrix per matrix

--- scripts/gridScript.cs
+++ scripts/gridScript.cs
@@ -6,8 +6,10 @@ using System.Linq;
 
 public partial class gridScript : Node2D
 {
-	//index is [y][x] value is 0 = empty, 1 = power, 2 = full, 3 = power full
+	//indexPos is [y][x] value is 0 = empty, 1 = power, 2 = full, 3 = power full
 	public Array<Array<int>> shipMatrix = new Array<Array<int>>();
+	public Array<Array<int>> powerMatrix = new Array<Array<int>>();
+	public Array<Array<int>> adjacentMatrix = new Array<Array<int>>();
 	public Array<int> mouseGridPosition = new Array<int>();
 	private int matrixSize = 10;//must be divisible by 2
 	private float gridScale = 64f;
@@ -15,28 +17,26 @@ public partial class gridScript : Node2D
 	public Boolean isBuilding = true;
 	public Array<int> currentGridPosition;
 	[Export] public Sprite2D module;
-	private void fillMatrix(Array<Array<int>> blockMatrix, Array<int> blockPosition, int fillType) {
+	[Export] public Node2D shipCore;
+	private void quadrantFill(Array<Array<int>> table, int amount, Array<int> indexPos) {
+		table[indexPos[1]+1][indexPos[0]] += amount;
+		table[indexPos[1]-1][indexPos[0]] += amount;
+		table[indexPos[1]][indexPos[0]+1] += amount;
+		table[indexPos[1]][indexPos[0]-1] += amount;
+	}
+	private void fillMatrix(Array<Array<int>> blockMatrix, Array<int> blockPosition, string fillType) {
 		for (int blockCount = 0; blockCount < blockMatrix.Count; blockCount++){
 			Array<int> currentBlock = blockMatrix[blockCount];
 			int blockYOffset = blockPosition[1]+matrixSize/2;
 			int blockXOffset = blockPosition[0]+matrixSize/2;
-			if(fillType == 4){
-				var blockValue = shipMatrix[currentBlock[1]+blockYOffset][currentBlock[0]+blockXOffset];
-				switch(blockValue){
-					case 0:
-					{
-						shipMatrix[currentBlock[1]+blockYOffset][currentBlock[0]+blockXOffset] = 1;
-						break;
-					}
-					case 2:
-					{
-						shipMatrix[currentBlock[1]+blockYOffset][currentBlock[0]+blockXOffset] = 3;
-						break;
-					}
-				}
+			if(fillType == "full"){
+				Array<int> indexPos = new Array<int>{currentBlock[0]+blockXOffset,currentBlock[1]+blockYOffset};
+				shipMatrix[indexPos[1]][indexPos[0]] = 1;
+				quadrantFill(powerMatrix,1,indexPos);
+				quadrantFill(adjacentMatrix,1,indexPos);
 			}
 			else{
-				shipMatrix[currentBlock[1]+blockYOffset][currentBlock[0]+blockXOffset] = fillType;
+				shipMatrix[currentBlock[1]+blockYOffset][currentBlock[0]+blockXOffset] = 1;
 			}
 		}
 	}
@@ -48,37 +48,47 @@ public partial class gridScript : Node2D
 		// returns the occupation or power of the space of the blockmatrix
 		Boolean occupied = false;
 		Boolean powered = false;
-
+		Boolean adjacent = false;
 		int blockYOffset = blockPosition[1]+matrixSize/2;
 		int blockXOffset = blockPosition[0]+matrixSize/2;
 
 		for (int blockCount = 0; blockCount < blockMatrix.Count; blockCount++){
 			Array<int> currentBlock = blockMatrix[blockCount];
 			int matrixValue = shipMatrix[currentBlock[1]+blockYOffset][currentBlock[0]+blockXOffset];
-			if(matrixValue == 1 || matrixValue == 3){
+			int powerValue = powerMatrix[currentBlock[1]+blockYOffset][currentBlock[0]+blockXOffset];
+			int adjacentValue = adjacentMatrix[currentBlock[1]+blockYOffset][currentBlock[0]+blockXOffset];
+			if(powerValue > 0){
 				powered = true;
 			}
-			if(matrixValue == 2 || matrixValue == 3){
+			if(matrixValue == 0){
 				occupied = true;
 			}
+			if(adjacentValue > 0){
+				adjacent = true;
+			}
 		}
-		if (occupied != true && powered != true){
+		if (occupied == true){
 			return 0;
 		}
-		if (occupied != true && powered == true){
+		if (occupied != true && powered != true && adjacent == true){
 			return 1;
 		}
-		if (occupied == true && powered != true){
+		if (occupied != true && powered == true && adjacent == true){
 			return 2;
-		}
-		if (occupied == true && powered == true){
-			return 3;
 		}
 		return -1;
 	}
 
 	public Boolean placeModule(moduleType type, Array<Array<int>> blockMatrix, Array<int> blockPosition){
 
+		int scan = scanMatrix(blockMatrix,blockPosition);
+		if(scan == 0 || scan == -1){
+			return false;
+		}
+		
+		if(type == moduleType.Reactor){
+			
+		}
 		return false;
 	}
 	public override void _Ready(){
@@ -88,13 +98,11 @@ public partial class gridScript : Node2D
 				xPlane.Add(0);
 			}
 			shipMatrix.Add(xPlane);
+			powerMatrix.Add(xPlane);
+			adjacentMatrix.Add(xPlane);
 		}
 
 		GD.Print(shipMatrix[5][2]);
-		shipMatrix[matrixSize/2][matrixSize/2] = 2;
-		shipMatrix[matrixSize/2][matrixSize/2+1] = 2;
-		shipMatrix[matrixSize/2+1][matrixSize/2] = 2;
-		shipMatrix[matrixSize/2+1][matrixSize/2+1] = 2;
 	}
 
 	public override void _Input(InputEvent @event){
